7970b7283a51370fd6bb89ba410fe2cd56c70215
badal8877 <badal110059@gmail.com>
2024-10-09 14:54:48 +0530

    notice

---
+++ src/models/noticeBoard.model.js
@@ -0,0 +1,26 @@
+import mongoose, {Schema} from "mongoose";
+
+const noticeBoardSchema = new Schema ({
+    Department: {
+        type: String,
+        required: true
+    },
+    HeadLine: {
+        type: String,
+        required: true
+    },
+    Description:{
+        type: String,
+        required: true
+    },
+    Document: {
+        type: String,
+        required: true
+    },
+    Publishing_Date: {
+        type: Date,
+        default: Date.now,
+    }
+});
+
+export const NoticeBoard = mongoose.model('NoticeBoard' , noticeBoardSchema);
