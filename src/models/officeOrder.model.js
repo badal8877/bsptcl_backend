a16d20ffe392b34cbae4a00f0353aea0528b612b
badal8877 <badal110059@gmail.com>
2024-10-16 14:12:51 +0530

    officenotification

---
+++ src/models/officeOrder.model.js
@@ -0,0 +1,26 @@
+import mongoose, { Schema } from "mongoose";
+
+const officeOrderSchema = new Schema({
+    Department: {
+        type: String,
+        required: true
+    },
+    Order_Ref_No: {
+        type: Number,
+        required: true
+    },
+    Order_Title: {
+        type: String,
+        required: true
+    },
+    Description: {
+        type: String,
+        required: true
+    },
+    Document: {
+        type: String,
+        required: true
+    }
+});
+
+export const OfficeOrder = mongoose.model('OfficeOrder', officeOrderSchema)
