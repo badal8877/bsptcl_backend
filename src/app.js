dbc6ffde4d799fec29867cb5a99ad3457ed98bf7
badal8877 <badal110059@gmail.com>
2024-11-15 11:27:03 +0530

    app

--- src/app.js
+++ src/app.js
@@ -13,7 +13,8 @@ app.use(cors({
 
 app.use(express.json({limit: "10mb"}))
 app.use(express.urlencoded({extended: true, limit: "10mb"}))
-app.use(express.static("public"))
+app.use('/public', express.static("public"));
+// app.use(express.static("public"))
 app.use(cookieParser())
 
 
